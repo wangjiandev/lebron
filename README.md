1f23af3e2d51e97187d8ec7a2911d10a81573119
wangjian <wangjian0504@gmail.com>
2022-07-17 10:47:32 +0800

    feat:通过命令行生成api和rpc代码示例

--- README.md
+++ README.md
@@ -17,3 +17,16 @@ lebron为工程名，lebron下面有apps和pkg两个目录，其中apps存放的
 - rmq - 负责进行流式任务处理，上游一般依赖消息队列，比如kafka等
 - admin - 也是对内的服务，区别于rpc，更多的是面向运营侧的且数据权限较高，通过隔离可带来更好的代码级别的安全，直接提供HTTP接口
 
+# 使用说明
+## rpc代码生成
+执行如下命令即可初始化order rpc代码
+```sh
+cd lebron/apps/order
+goctl rpc new rpc
+```
+## api代码生成
+执行如下命令即可初始化order admin代码，注意order admin为`api服务`，直接对前端提供HTTP接口
+```sh
+goctl api new admin
+```
+
---
+++ apps/order/admin/internal/handler/adminhandler.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"lebron/apps/order/admin/internal/logic"
+	"lebron/apps/order/admin/internal/svc"
+	"lebron/apps/order/admin/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func AdminHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewAdminLogic(r.Context(), svcCtx)
+		resp, err := l.Admin(&req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
